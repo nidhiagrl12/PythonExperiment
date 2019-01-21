9fc39f3250be9f9a21c9a44ad7a4856dee36f4e4
Nikhil Barthwal <nbarthwal@google.com>
2019-01-20 18:10:07 -0800

    .

---
+++ presubmit-tests.sh
@@ -0,0 +1,9 @@
+pip3 install virtualenv
+
+virtualenv -p python3 sandbox
+
+source sandbox/bin/activate
+
+python src/test.py
+
+deactivate
--- src/demo.py
+++ src/demo.py
@@ -1,3 +1,13 @@
-dssfsa
+from typing import List, Tuple
+
+
+# Declare a point type annotation using a tuple of ints of [x, y]
+Point = Tuple[int, int]
+
+
+# Create a function designed to take in a list of Points
+def print_points(points: List[Point]):
+    for point in points:
+        print("X:", point[0], "  Y:", point[1])
 
 
