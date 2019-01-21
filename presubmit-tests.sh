e7ad1ce9fea4c63623c4e4a3c6fb1a7ba89d6cfa
Nikhil Barthwal <nbarthwal@google.com>
2019-01-20 23:33:06 -0800

    fix script

--- presubmit-tests.sh
+++ presubmit-tests.sh
@@ -1,34 +1,34 @@
 
 function check() {
 
-	local exit_code=0
-	pip3 install virtualenv
-	
-	virtualenv -p python3 sandbox
-	
-	source sandbox/bin/activate
-	
-	pip3 install -r requirements.txt
-	
-	#python src/test.py
-	
-	mypy src/ test/ > errors.txt
-	
-	if [ -s errors.txt ]
-	then
-	  awk 'BEGIN { print "\n"} { print $0} END { print "\n" NR " Type Errors found"}' < errors.txt
-	  exit_code=1
-	else
-	  PYTHONPATH=./src:./test python test/t1.py
-	  exit_code=$?
-	fi
-	
-	deactivate
-	
-	return $exit_code
+  local exit_code=0
+  pip3 install virtualenv
+
+  virtualenv -p python3 sandbox
+  source sandbox/bin/activate
+  pip3 install -r requirements.txt
+
+  mypy src/ test/ > errors.txt
+
+  if [ -s errors.txt ]
+  then
+    awk 'BEGIN { print "\n"} { print $0} END { print "\n" NR " Type Errors found"}' < errors.txt
+    exit_code=1
+  else
+    ls test/*.py | PYTHONPATH=./src:./test xargs python
+    exit_code=$?
+  fi
+
+  deactivate
+
+  return $exit_code
 }
 
 check
+readonly exit_code=$?
+
 rm -rf sandbox
 rm -rf errors.txt
-exit $?
+rm -rf */__pycache__
+
+exit $exit_code
